2c5af3fea042a31b94120f5423f102b114843bec
AxelPanic <alexthickey@gmail.com>
2023-04-11 10:48:31 -0500

    webpack setup from bcs work

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,60 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "markdown"
+      }),
       
-    ],
+    new CopyWebpackPlugin({
+      patterns: [{ from: "favicon.ico", to: "favicon.ico" }],
+    }),
+
+    new InjectManifest({
+      swSrc: "./src-sw.js",
+      swDest: "src-sw.js",
+    }),
+
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: "markup",
+      short_name: "markup",
+      description: "mark it up or type it down!",
+      background_color: "black",
+      theme_color: "black",
+      start_url: "/",
+      publicPath: "/",
+      icons: [
+        {
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
+    }),
+  ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.scc$/i,
+          use: ["style-loader", "scc-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "bable-loader",
+            options: {
+              presets: ["@bable/preset.env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ]
+            }
+          }
+        }
       ],
     },
   };
