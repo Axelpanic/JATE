751a5e9280b368e73e2ccb981b382d1a40e5ca5b
AxelPanic <alexthickey@gmail.com>
2023-04-17 12:03:59 -0500

    Completed TODO in js folder, ran test. Fixed scripts inside of package Json for all folders. added my own info to the author and completed description. completed webpack file and src sw.

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {console.log("PUT to the database");
+const markupDB = await openDB("markupDB", 1);
+const tx = markupDB.transaction("markup", "readwrite");
+const store = tx.objectStore("markup");
+const request = store.put({ id: 1, content });
+const result = await request;
+console.log("data saved to the database", result);
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {console.log("GET from the database");
+const markupDB = await openDB("markupDB", 1);
+const tx = markupDB.transaction("markup", "readonly");
+const store = tx.objectStore("markup");
+const request = store.get(1);
+const result = await request;
+console.log("data received from the database");
+return result;
+};
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,50 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Check onload to see if the app is supported
+window.addEventListener("load", () => {
+    if (!installSupported()) {
+      console.log("Install is not supported");
+      hideInstallButton();
+      return;
+    }
+  
+    console.log("Install is supported");
+    window.addEventListener("beforeinstallprompt", (event) => {
+      window.deferredPrompt = event;
+      showInstallButton(event);
+    });
+  
+    butInstall.addEventListener("click", async () => {
+      const promptEvent = window.deferredPrompt;
+  
+      if (!promptEvent) {
+        return;
+      }
+  
+      // Show prompt
+      promptEvent.prompt();
+  
+      window.deferredPrompt = null;
+    });
+  
+    // Hide the install button
+    window.addEventListener("appinstalled", (event) => {
+      hideInstallButton();
+      window.deferredPrompt = null;
+    });
+  });
+  
+  /** Checks to see if install is supported in browser */
+  function installSupported() {
+    return "getInstalledRelatedApps" in navigator;
+  }
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+  function showInstallButton(event) {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle("hidden", false);
+  }
+  function hideInstallButton() {
+    butInstall.classList.toggle("hidden", true);
+  }
