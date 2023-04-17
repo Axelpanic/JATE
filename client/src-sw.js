1c7c4fa8a12e8a9e8bd402e495a622096d690fea
AxelPanic <alexthickey@gmail.com>
2023-04-17 13:09:43 -0500

    fixed spelling in client folder and fixed stalling

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,14 +1,14 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst, StaleWhileRevalidate } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
     new CacheableResponsePlugin({
       statuses: [0, 200],
@@ -20,27 +20,25 @@ const pageCache = new CacheFirst({
 });
 
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
   ({ request }) => ["style", "script", "worker"].includes(request.destination),
-  // static cache
   new StaleWhileRevalidate({
     cacheName: "static-resources",
     plugins: [
-      //adding max age cache
+      // maximum age of 30 days
       new CacheableResponsePlugin({
         statuses: [0, 200],
       }),
-      //max age set to 30 days
       new ExpirationPlugin({
-        maxEntries: 100,
-        maxAgeSeconds: 30 * 24 *60 *60, //30 days
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
       })
     ],
   })
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
 
 module.exports = () => {
   return {
@@ -18,54 +17,52 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./index.html",
-        title: "JATE"
+        template: './index.html',
+        title: 'jate'
       }),
-      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'jate',
+        short_name: 'jate',
+        description: 'Just Another Text Editor',
+        background_color: 'black',
+        theme_color: 'black',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+    ],
 
-    new InjectManifest({
-      swSrc: "./src-sw.js",
-      swDest: "src-sw.js",
-    }),
-
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: true,
-      name: "Just Another Text Editor",
-      short_name: "JATE",
-      description: "JATE!",
-      background_color: "black",
-      theme_color: "black",
-      start_url: "/",
-      publicPath: "/",
-      icons: [
-        {
-          src: path.resolve("src/images/logo.png"),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join("assets", "icons"),
-        },
-      ],
-    }),
-  ],
-// css loader and babel
+// CSS loaders and babel
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@bable/preset.env"],
+              presets: ['@babel/preset-env'],
               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            }
-          }
-        }
+            },
+          },
+        },
       ],
-    },
+    }
   };
 };
