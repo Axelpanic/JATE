be9527e01d274f99105647d5ef607692b6e97482
AxelPanic <alexthickey@gmail.com>
2023-04-17 12:15:10 -0500

    removed fancy favicon stuff, can't figure it out

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -28,8 +28,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({ request}) =>
-  request.destination === "style" || request.destination ==="script",
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
     cacheName: "static-resources",
     plugins: [
@@ -37,21 +36,5 @@ registerRoute(
         statuses: [0, 200],
       }),
     ],
-  })
-);
-
-registerRoute(
-  ({ request }) => request.destination ==="image",
-  new CacheFirst({
-    cacheName: "image-cache",
-    plugins: [
-      new CacheableResponsePlugin({
-      statuses: [o, 200],
-    }),
-    new ExpirationPlugin({
-      maxEntries:60,
-      maxAgeSeconds: 30 * 24 * 60 * 60,
-    }),
-    ]
   })
 );
